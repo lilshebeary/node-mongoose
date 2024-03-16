a286524b4d4031becd4987336c0cb4c5e60b2a21
lilshebeary <cirijaye@gmail.com>
2024-03-16 04:10:04 -0700

    set up

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Campsite = require('./models/campsite');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+connect.then(() => {
+    const newCampsite = new Campsite({
+        name: 'React Lake Campground',
+        description: 'test'
+    });
+
+    newCampsite.save()
+    .then(campsite => {
+        console.log(campsite);
+        return Campsite.find();
+    })
+    .then(campsites => {
+        console.log(campsites);
+        return Campsite.deleteMany();
+    })
+    .then(campsites => {
+        return mongoose.connection.close();
+    })
+    .catch(err => {
+        console.log(campsites);
+        mongoose.connection.close();
+    })
+})
